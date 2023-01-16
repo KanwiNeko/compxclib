1901057ea7b9527015ed273ed1d9eb441bfb4f0e
kanwiWindows <business@kanwi.gay>
2023-01-16 17:40:33 -0500

    Lexer finished in its entirety

--- src/main/kotlin/compxclib/parser/ComplexLexer.kt
+++ src/main/kotlin/compxclib/parser/ComplexLexer.kt
@@ -1,26 +1,141 @@
 package compxclib.parser
 
 import compxclib.ComplexNumber
-import compxclib.Sign
 
-internal object ComplexLexer {
+object ComplexLexer {
+
+    // static variables used for checking
+    private val leftParenthesisToken = Pair(Tokens.STRUCTURE, "(")
+    private val rightParenthesisToken = Pair(Tokens.STRUCTURE, ")")
+    private val positiveOperatorToken = Pair(Tokens.OPERATOR, "+")
+    private val numbers = arrayOf(
+        Tokens.REAL_NUMBER,
+        Tokens.IMAGINARY_NUMBER
+    )
+
+    // dynamic variables using during logic
     private lateinit var tokenList: MutableComplexList
-    private var lastEatenToken: TokenTuple? = null
     private var index = 0
 
-    fun eat(): Pair<TokenTuple, Int> {
+    // parses a string into a double
+    private fun parseDouble(input: String): Double {
+        val sign: Double = when(input[0]) {
+            '+' -> 1.0
+            '-' -> -1.0
+            else -> 0.0
+        }
+        val number: Double = input
+            .removeRange(0 until 1)
+            .toDouble()
+        return number * sign
+    }
+
+    // returns the token to each side of the given token
+    private fun neighbors(index: Int): Pair<TokenTuple?, TokenTuple?> {
+        return Pair(
+            try {
+                tokenList[index-1]
+            } catch(_: IndexOutOfBoundsException) {
+                null
+            },
+            try {
+                tokenList[index+1]
+            } catch (_: IndexOutOfBoundsException) {
+                null
+            }
+        )
+    }
+
+    // returns the first element of the list
+    private fun at(): TokenTuple {
+        return tokenList[0]
+    }
+
+    // removes first token in the list, adds the eaten token to the lastEatenToken variable and returns the token
+    private fun eat(): TokenTuple {
         val eatenToken = tokenList[0]
         tokenList.removeAt(0)
-        this.lastEatenToken = eatenToken
         index++
-        return Pair(eatenToken, index)
+        return eatenToken
+    }
+
+    // deletes stray operators; ex: ln(4+) -> ln(4) or sin(+2) -> sin(2) or +4 -> 4
+    private fun cleanTokenList(): Int {
+        val removedIndexes = mutableListOf<Int>()
+        for ((i, v) in tokenList.zip(0 until tokenList.size)) {
+            val (leftNeighbor, rightNeighbor) = neighbors(v)
+            if(i.first != Tokens.OPERATOR) continue
+            if (leftNeighbor == leftParenthesisToken || leftNeighbor == null) removedIndexes += v
+            if (rightNeighbor == rightParenthesisToken || rightNeighbor == null) removedIndexes += v
+        }
+        removedIndexes.reverse()
+        for (i in removedIndexes) {
+            tokenList.removeAt(i)
+        }
+        return removedIndexes.size
     }
 
     fun complexLexer(complexList: ComplexList): Pair<ComplexList, List<ComplexNumber>> {
+        // Assignments
         this.tokenList = complexList.toMutableList()
-        val returnedComplexList: List<ComplexNumber> = mutableListOf()
+        val returnedList: MutableComplexList = mutableListOf()
+        val returnedComplexList: MutableList<ComplexNumber> = mutableListOf()
+
+        // Pre-lexing logic
+        var elementsRemoved: Int? = null
+        while (elementsRemoved != 0) {
+            elementsRemoved = cleanTokenList()
+        }
+
+        // Main Logic
         while (tokenList.isNotEmpty()) {
+            if(at().first !in numbers) { returnedList += eat(); continue }
+
+            var real: Double? = null
+            var imaginary: Double? = null
+            val number = eat()
+            when (number.first) {
+                Tokens.REAL_NUMBER -> { real = parseDouble(number.second) ; imaginary = null }
+                Tokens.IMAGINARY_NUMBER -> { imaginary = parseDouble(number.second) ; real = null}
+                else -> Unit
+            }
+
+            val sign = eat()
+            if (sign == positiveOperatorToken) {
+                @Suppress("KotlinConstantConditions")
+                when (null) {
+                    real -> {
+                        real = if (at().first == Tokens.REAL_NUMBER) {
+                            returnedList += Pair(Tokens.COMPLEX_NUMBER, "${returnedComplexList.size}")
+                            parseDouble(eat().second)
+                        } else {
+                            returnedList += positiveOperatorToken
+                            0.0
+                        }
+                    }
+                    @Suppress("") imaginary -> {
+                        imaginary = if (at().first == Tokens.IMAGINARY_NUMBER) {
+                            returnedList += Pair(Tokens.COMPLEX_NUMBER, "${returnedComplexList.size}")
+                            parseDouble(eat().second)
+                        } else {
+                            returnedList += positiveOperatorToken
+                            0.0
+                        }
+                    }
+                }
+            } else {
+                @Suppress("KotlinConstantConditions")
+                when (null) {
+                    real -> real = 0.0
+                    imaginary -> imaginary = 0.0
+                }
+                returnedList += Pair(Tokens.COMPLEX_NUMBER, "${returnedComplexList.size}")
+                returnedList += sign
+            }
+
+            returnedComplexList += ComplexNumber(real!!, imaginary!!)
 
         }
+        return Pair(returnedList, returnedComplexList)
     }
 }
--- src/test/kotlin/compxclib/ParserTest.kt
+++ src/test/kotlin/compxclib/ParserTest.kt
@@ -1,16 +1,19 @@
-package compxclib.parser
+package compxclib
 
-import compxclib.ComplexNumber
-import compxclib.functions.ln
+import compxclib.parser.ComplexLexer
+import compxclib.parser.Lexer
+import compxclib.parser.NumberLexer
 import kotlin.test.*
 
 class ParserTest {
     @Test
     fun parserTest() {
-        val firstTokenization = Lexer.tokenize("ln(24+i98)")
+        val firstTokenization = Lexer.tokenize("*4 + 5i + 6 * ln(i8)")
         NumberLexer.init(firstTokenization)
         val secondTokenization = NumberLexer.numberLexer()
-        println(secondTokenization)
+        val (thirdTokenization, numbers) = ComplexLexer.complexLexer(secondTokenization)
+        println("Tokens: $thirdTokenization")
+        println("Numbers: $numbers")
         assertEquals(true, true)
     }
 }
