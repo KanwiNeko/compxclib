44bdb4fe9002222f9ea6913753dbfd5830875afe
kanwiWindows <business@kanwi.gay>
2023-01-18 18:14:10 -0500

    Fixed git tracking issue

--- src/test/kotlin/compxclib/ParserTest.kt
+++ src/test/kotlin/compxclib/ParserTest.kt
@@ -2,14 +2,15 @@ package compxclib
 
 import compxclib.functions.sin
 import compxclib.parser.Parser
+import kotlin.math.PI
 import kotlin.test.*
 
 class ParserTest {
     @Test
     fun parserTest() {
-        Constants.setDefaultRound(10)
-        val expected = sin(ComplexNumber(34*34, 0))*ComplexNumber(0, 8)
-        val input = "sin(34^2)*8i"
+        //Constants.setDefaultRound(10)
+        val expected = ComplexNumber(-1, 0)
+        val input = "exp($PI i)"
         val parser = Parser(input)
         val result = parser.parse()
         println("[INFO]: Expecting result $expected")
